e8f3110618a17577850ffd974ea5f96508d28e2e
Eliasnew52 <xxeliasnew52xx@gmail.com>
2024-02-01 23:28:19 -0600

    Proyect Finished (Frontend still looks goofy af)

--- mail/mail/mail/static/mail/inbox.js
+++ mail/mail/mail/static/mail/inbox.js
@@ -27,8 +27,8 @@ function compose_email() {
 }
 
 
-function view_email(id){
-    fetch(`/emails/${id}`)
+function view_email(id) {
+  fetch(`/emails/${id}`)
     .then(response => response.json())
     .then(email => {
       console.log(email);
@@ -37,7 +37,7 @@ function view_email(id){
       document.querySelector('#compose-view').style.display = 'none';
       document.querySelector('#emails-detail').style.display = 'block';
 
-      document.querySelector('#emails-detail').innerHTML=`
+      document.querySelector('#emails-detail').innerHTML = `
       <ul class="list-group">
         <li class="list-group-item"><strong>From:</strong>${email.sender}</li>
         <li class="list-group-item"><strong>To:</strong>${email.recipients}</li>
@@ -46,7 +46,51 @@ function view_email(id){
         <li class="list-group-item">${email.body}</li>
       </ul>
       `
-    
+      //Check whether a Mail is Read or Not
+      if (!email.read) {
+        fetch(`emails/${email.id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            read: true
+          })
+        })
+      }
+
+      //Archive - UnArchive JS
+      const Arch_Btn = document.createElement('button');
+      Arch_Btn.innerHTML = email.archived ? "Unarchive" : "Archive";
+      Arch_Btn.className = email.archived ? "btn btn-success m-3" : "btn btn-warning m-3";
+      Arch_Btn.addEventListener('click', function () {
+        console.log("This Button has been Clicked")
+        fetch(`emails/${email.id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            archived: !email.archived
+          })
+        })
+          .then(() => { load_mailbox('archive') })
+      });
+      document.querySelector('#emails-detail').append(Arch_Btn);
+
+      //Reply
+      const Reply_Btn = document.createElement('button');
+      Reply_Btn.innerHTML = "Reply";
+      Reply_Btn.className = "btn btn-info m-3";
+      Reply_Btn.addEventListener('click', function () {
+        console.log("reply")
+        compose_email();
+
+        document.querySelector('#compose-recipients').value = email.sender;
+        let subject = email.subject;
+        if(subject.split('',1)[0] != "Re:"){
+          subject = "Re:" + email.subject;
+        }
+        document.querySelector('#compose-subject').value = subject ;
+        document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} Wrote ${email.body}`;
+
+
+      });
+      document.querySelector('#emails-detail').append(Reply_Btn);
     })
 
 }
@@ -63,32 +107,32 @@ function load_mailbox(mailbox) {
 
   //Retrieve Mails for Mailbox
   fetch(`/emails/${mailbox}`)
-  .then(response => response.json())
-  .then(emails => {
-    //Mail Creation
-    // Sender - Subject - Timestamp
-    emails.forEach(mail => {
-      console.log(mail);
+    .then(response => response.json())
+    .then(emails => {
+      //Mail Creation
+      // Sender - Subject - Timestamp
+      emails.forEach(mail => {
+        console.log(mail);
 
-      //Element Creation
-      const newMail = document.createElement('div');
-      newMail.className = "list-group-item my-3 mx-2";
-      newMail.innerHTML = `
+        //Element Creation
+        const newMail = document.createElement('div');
+        newMail.className = "list-group-item my-3 mx-2";
+        newMail.innerHTML = `
         <h5>From: ${mail.sender}</h5>
         <p>Subject: ${mail.subject}</p>
         <p>${mail.timestamp}</p>
       `;
-      
-      //BG Color (if read is true, we use it, else, we working with unread)
-      newMail.classList.add(mail.read ? 'read' : 'unread');
 
-      //Click event
-      newMail.addEventListener('click', function(){
-        view_email(mail.id)
-      }); 
-      document.querySelector('#emails-view').append(newMail);
+        //BG Color (if read is true, we use it, else, we working with unread)
+        newMail.classList.add(mail.read ? 'read' : 'unread');
+
+        //Click event
+        newMail.addEventListener('click', function () {
+          view_email(mail.id)
+        });
+        document.querySelector('#emails-view').append(newMail);
+      })
     })
-  })
 
 }
 
